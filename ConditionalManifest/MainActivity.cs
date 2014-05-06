983076d48b883acdd7e284699f6e1e59076f8a55
Chris Riesgo <chrisriesgo@Chriss-MacBook-Pro-2.local>
2014-05-06 08:43:40 -0500

    Initial commit

---
+++ ConditionalManifest/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ConditionalManifest
+{
+	#if DEBUG
+	[Activity(Label = "Conditional Manifest DEBUG", MainLauncher = true)]
+	#else
+	[Activity(Label = "Conditional Manifest", MainLauncher = true)]
+	#endif
+	public class MainActivity : Activity
+	{
+		// 	TODO:	See the csproj file Debug Conditional:
+		//	I expect this to evaluate and overwrite which manifest template to use
+		//	<AndroidManifest>Properties\AndroidManifestDebug.xml</AndroidManifest>
+
+		int count = 1;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			
+			button.Click += delegate
+			{
+				button.Text = string.Format("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+
