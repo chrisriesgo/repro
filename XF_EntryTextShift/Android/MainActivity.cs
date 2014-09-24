5176e0ca0746ceb26132495534db07d7d5bd570a
chrisriesgo <cmriesgo@gmail.com>
2014-09-24 08:50:11 -0500

    Initial commit

---
+++ XF_EntryTextShift/Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+
+namespace EntryTextShift.Android
+{
+	[Activity(Theme="@style/Theme.Forms", MainLauncher = true, LaunchMode = LaunchMode.SingleTask,
+		ConfigurationChanges = ConfigChanges.KeyboardHidden | ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+	public partial class MainActivity : AndroidActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			if(!Xamarin.Forms.Forms.IsInitialized)
+				Xamarin.Forms.Forms.Init (this, bundle);
+			SetPage(App.GetMainPage());
+
+			ActionBar.SetIcon(Resource.Drawable.Icon);
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+
+			App.Navigation.PushModalAsync(App.GetModalPage());
+		}
+	}
+}
+
---
+++ XF_EntryTextShift/EntryTextShift/App.cs
@@ -0,0 +1,47 @@
+﻿using Xamarin.Forms;
+
+namespace EntryTextShift
+{
+	public class App
+	{
+		public static INavigation Navigation { get; internal set; }
+
+		public static Page GetMainPage()
+		{
+			var page = new ContentPage();
+			var rootNav = new NavigationPage(page);
+
+			// Attention! -- commenting out this line "fixes" the issue
+			NavigationPage.SetHasNavigationBar(page, false);
+
+			Navigation = rootNav.Navigation;
+			return rootNav;
+		}
+
+		public static Page GetModalPage()
+		{	
+			return new NavigationPage(new ModalPage());
+		}
+	}
+
+	public class ModalPage : ContentPage
+	{
+		public ModalPage()
+		{
+			BackgroundColor = Color.Black;
+			Content = new StackLayout {
+				Padding = new Thickness(0, 0, 0, 0),
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				Children = {
+					new Entry { 
+						Placeholder = "Email Address", 
+						HeightRequest = 60.0f,
+						BackgroundColor = Color.White,
+						TextColor = Color.Gray,
+					},
+				}
+			};
+		}
+	}
+}
+
