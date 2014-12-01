d8f92e656d6bc485665b2c608788efd43d9b065a
chrisriesgo <cmriesgo@gmail.com>
2014-11-30 19:14:41 -0600

    Added Lollipop test case

---
+++ XF_TestLollipopForms/Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+
+namespace TestLollipopForms.Android
+{
+	[Activity(Label = "TestLollipopForms.Android.Android", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : AndroidActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			Xamarin.Forms.Forms.Init(this, bundle);
+
+			SetPage(App.GetAndroidNavigationPage());
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+
+			App.Navigation.PushModalAsync(App.GetAnotherPage());
+		}
+	}
+}
+
---
+++ XF_TestLollipopForms/TestLollipopForms/App.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+
+namespace TestLollipopForms
+{
+	public class App
+	{
+		public static INavigation Navigation { get; internal set; }
+
+		public static NavigationPage GetAndroidNavigationPage()
+		{
+			var page = GetMainPage();
+			var rootNav = new NavigationPage(page);
+
+			Navigation = rootNav.Navigation;
+
+			return rootNav;
+		}
+
+		public static Page GetMainPage()
+		{	
+			var page = new ContentPage { 
+				Content = new Label {
+					Text = "Hello, Forms!",
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+				},
+			};
+
+			return page;
+		}
+
+		public static Page GetAnotherPage()
+		{
+			var page = new ContentPage { 
+				Content = new Label {
+					Text = "Forms Modal",
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+				},
+			};
+			return page;
+		}
+	}
+}
+
