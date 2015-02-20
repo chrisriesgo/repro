6829a20a1419c08532af66e7e59384c21dae8b6c
chrisriesgo <cmriesgo@gmail.com>
2015-02-20 16:22:02 -0600

    Repro for TapGestureRecognizer BindingContext issue

---
+++ XF_1_3_4_CellBindingContext/XF_1_3_4_CellBindingContext/CustomCell.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+namespace XF_1_3_4_CellBindingContext
+{
+	public class CustomCell : ViewCell
+	{
+		public Label DescriptionLabel { get; set; }
+		public Image Icon { get; set; }
+		public TapGestureRecognizer TapRecognizer { get; set; }
+		EventHandler _iconTapped;
+
+		public CustomCell()
+		{
+			DescriptionLabel = new Label 
+			{
+				TextColor = Color.Black,
+				FontSize = 14
+			};
+
+			DescriptionLabel.SetBinding(Label.TextProperty,
+				new Binding("."));
+
+			Icon = new Image 
+			{
+				BackgroundColor = Color.Red,
+				WidthRequest = 30,
+				HeightRequest = 30,
+				VerticalOptions = LayoutOptions.Start,
+				HorizontalOptions = LayoutOptions.End
+			};
+
+			View = new StackLayout {
+				Orientation = StackOrientation.Vertical,
+				HorizontalOptions = LayoutOptions.StartAndExpand,
+				Children = {
+					Icon,
+					DescriptionLabel
+				}
+			};
+
+			TapRecognizer = new TapGestureRecognizer();
+			TapRecognizer.NumberOfTapsRequired = 1;
+			_iconTapped = (sender, e) =>
+			{
+				var msg = BindingContext as string;
+				Console.WriteLine(String.Format("\nBindingContext Cell '{0}'", msg));
+			};
+			TapRecognizer.Tapped += _iconTapped;
+
+			Icon.GestureRecognizers.Add(TapRecognizer);
+		}
+
+		string _stash;
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			var msg = BindingContext as string;
+			var json = JsonConvert.SerializeObject(msg);
+			_stash = JsonConvert.DeserializeObject<string>(json);
+			if(TapRecognizer != null)
+			{
+				TapRecognizer.Tapped -= _iconTapped;
+				_iconTapped = (sender, e) =>
+				{
+					this.BindingContext = _stash;
+					Console.WriteLine(String.Format("\nBindingContext Cell '{0}'", msg));
+				};
+				TapRecognizer.Tapped += _iconTapped;
+				TapRecognizer.BindingContext = _stash;
+			}
+
+			Console.WriteLine("OnAppearing Row is: " + msg);
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+		}
+
+//		void IconTapped(object sender, EventArgs e)
+//		{
+//			var msg = BindingContext as string;
+//			Console.WriteLine(String.Format("\nBindingContext Cell '{0}'", msg));
+//		}
+	}
+}
+
---
+++ XF_1_3_4_CellBindingContext/XF_1_3_4_CellBindingContext/MainPage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XF_1_3_4_CellBindingContext
+{
+	public class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			BindingContext = new ViewModel();
+
+			var list = new ListView {
+				HasUnevenRows = true,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				RowHeight = -1,
+				ItemTemplate = new DataTemplate(typeof(CustomCell)),
+			};
+
+			list.SetBinding<ViewModel>(ItemsView<Cell>.ItemsSourceProperty, vm => vm.Items);
+			list.ItemTapped += HandleItemTapped;
+
+			Content = list;
+		}
+
+		void HandleItemTapped (object sender, ItemTappedEventArgs e)
+		{
+			var list = sender as ListView;
+			var vm = BindingContext as ViewModel;
+			var item = vm.Items.FindIndex(m => m == e.Item);
+			var a = vm.Items.FirstOrDefault(i => i == e.Item);
+			Console.WriteLine("Tapped: " + item.ToString());
+			list.SelectedItem = null;
+		}
+	}
+
+	public class ViewModel// : ObservableBase
+	{
+		public ViewModel()
+		{
+			Items = new List<string>();
+			for(var i = 1; i <= 100; i++)
+			{
+				Items.Add(i.ToString());
+			}
+		}
+
+		private List<string> _items;
+		public List<string> Items { get; set; }
+//		public List<string> Items
+//		{
+//			get
+//			{
+//				return _items;
+//			}
+//			set
+//			{
+//				SetField(ref _items, value);
+//			}
+//		}
+	}
+}
+
